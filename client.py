f6a847a20161d8f62c5491d7a56dfae889656275
silviamaa <silvia_cok92@hotmail.com>
2014-11-17 18:50:21 +0100

    programa avanzado

--- client.py
+++ client.py
@@ -5,27 +5,49 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+METODO = sys.argv[1]
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+#Partimos los argumentos
+z = sys.argv[2].split(":")
+login = z[-2]
+a = sys.argv[2].split("@")
+b = a[1].split(":")
+IPreceptor = b[-2]
+puertoSIP = b[-1]
+
+# Contenido que vamos a enviar si es un INVITE o un BYE
+MENSAJE =  METODO + " " + login
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IPreceptor, int(puertoSIP)))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+if (METODO == "INVITE"):
+    print "Enviando: " + MENSAJE
+    my_socket.send(MENSAJE + '\r\n')
+    data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+    print 'Recibido -- \r\n\r\n', data
+    
+    #Se envía el ACK
+    mensaje_ack = "ACK" + " " + login
+    my_socket.send(mensaje_ack + '\r\n')
+
+if (METODO == "BYE"):
+    print "Enviando: " + MENSAJE
+    my_socket.send(MENSAJE + '\r\n')
+    data = my_socket.recv(1024)
+
+    print 'Recibido -- \r\n\r\n', data
+
+    if (data == "SIP/2.0 200 OK"):
+        print "Hemos recibido el 200 OK"
 
 # Cerramos todo
 my_socket.close()
