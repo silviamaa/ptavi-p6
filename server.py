1a7672fde863262187c8829dc3f8ff31723863dd
alvaromv83 <alvaromv83@gmail.com>
2014-11-17 11:14:00 +0100

    Modificado server.py - Excepciones y recepción de métodos

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
+import os
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -13,19 +15,42 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
+        """
+        Método para recibir en el manejador y establecer comunicación SIP
+        """
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            else:
+                print "El cliente nos manda " + line
+                parameters = line.split()
+                method = parameters[0]
+                user = parameters[1].split(':')[1]
+                version = parameters[2]
+                
+                if method == 'INVITE':
+                    print method
+                elif method == 'BYE':
+                    print method
 
 if __name__ == "__main__":
+    try:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        AUDIO_FILE = sys.argv[3]
+        if not os.path.isfile(AUDIO_FILE):
+            raise IndexError
+    except IndexError:
+        print ("Usage: python server.py IP port audio_file")
+        raise SystemExit
+
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
