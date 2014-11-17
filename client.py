e9300dd9ba01a1345fc5de931e139fe613c88aa5
alvaromv83 <alvaromv83@gmail.com>
2014-11-17 11:13:02 +0100

    Modificado client.py - Parámetros shell y envío de método

--- client.py
+++ client.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa cliente que abre un socket a un servidor y envía mensajes SIP
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+# Cliente SIP.
 
 # Dirección IP del servidor.
 SERVER = 'localhost'
-PORT = 6001
+
+# Parámetros del usuario.
+try:
+    METHOD = sys.argv[1].upper()
+    parameters = sys.argv[2]
+except IndexError:
+    print ("Usage: python client.py method receiver@IP:SIPport")
+    raise SystemExit
+
+USER = parameters.split(':')[0]
+IP = USER.split('@')[1]
+PORT = int(parameters.split(':')[1])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+REQUEST = METHOD + " " + "sip:" + USER + " SIP/2.0\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+print "Enviando:\n" + REQUEST
+my_socket.send(REQUEST)
+try:
+    data = my_socket.recv(1024)
+except socket.error:
+    print ("Error: No server listening at " + IP + " port " + str(PORT))
+    raise SystemExit
 
 print 'Recibido -- ', data
 print "Terminando socket..."
