f9e0e77ec0b0e763e968343f382fe8eb62c0f38f
alvaromv83 <alvaromv83@gmail.com>
2014-11-17 15:56:50 +0100

    depurado client.py

--- client.py
+++ client.py
@@ -26,13 +26,14 @@ PORT = int(parameters.split(':')[1])
 VERSION = "SIP/2.0"
 
 # Contenido que vamos a enviar
-request = METHOD + " " + "sip:" + USER + " " + VERSION + "\r\n"
+request = METHOD + " " + "sip:" + USER + " " + VERSION + "\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
+# Enviamos solicitud y recibimos respuesta
 print "Enviando:\n" + request
 my_socket.send(request)
 try:
@@ -48,7 +49,7 @@ response = VERSION + " 100 Trying\r\n\r\n"
 response += VERSION + " 180 Ring\r\n\r\n"
 response += VERSION + " 200 OK\r\n\r\n"
 if data == response:
-    request = "ACK " + "sip:" + USER + " " + VERSION + "\r\n"
+    request = "ACK " + "sip:" + USER + " " + VERSION + "\r\n\r\n"
     print "Enviando:\n" + request
     my_socket.send(request)
 
