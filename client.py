85d3d3643411dc5d4cdc301ae45d602c88fbe05b
silvia marin mateos <smarin@iota22.aulas.gsyc.es>
2014-11-17 10:52:53 +0100

    INVITE hecho

--- client.py
+++ client.py
@@ -5,28 +5,39 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+METODO = sys.argv[1]
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+#Partimos los argumentos
+z = sys.argv[2].split(":")
+login = z[-2]
+a = sys.argv[2].split("@")
+b = a[1].split(":")
+IPreceptor = b[-2]
+puertoSIP = b[-1]
+
+
+# Contenido que vamos a enviar si es un INVITE
+LINE =  METODO + " " + login
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IPreceptor, int(puertoSIP)))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+if (METODO == "INVITE"):
+	print "Enviando: " + LINE
+	my_socket.send(LINE + '\r\n')
+	data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+	print 'Recibido -- \r\n\r\n', data
+
 
 # Cerramos todo
 my_socket.close()
 print "Fin."
+
