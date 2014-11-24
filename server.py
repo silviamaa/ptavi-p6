d2a9ce34ce694c8a5072c44205eed48715bbaac5
mireya elisabet valenzuela carballo <mireya@iota13.aulas.gsyc.es>
2014-11-24 10:15:57 +0100

    funciona menos rtp

--- server.py
+++ server.py
@@ -3,9 +3,17 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import sys
 import SocketServer
+import os
 
+try: 
+    UA = sys.argv[0] = 'server.py'
+    IP = sys.argv[1]
+    PUERTO = int(sys.argv[2])
+    FICHERO = sys.argv[3]
+except IndexError:
+    sys.exit("Usage: python server.py IP port audio_file")
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -14,18 +22,38 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+      
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
-
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            Metod = line.split(' ')[0]
+            self.wfile.write("Hemos recibido un " + Metod + '\r\n\r\n')
+            print "El cliente nos manda " + line
+            if not Metod in Metodos:
+                self.wfile.write('SIP/2.0 405 Method Not Allowed' + '\r\n\r\n')
+            elif Metod == 'INVITE':
+                rcv_invite = 'SIP/2.0 100 Trying' + '\r\n\r\n'
+                rcv_invite += 'SIP/2.0 180 Ring' + '\r\n\r\n'
+                rcv_invite += 'SIP/2.0 200 OK' + '\r\n\r\n'
+                self.wfile.write(rcv_invite)
+            elif Metod == 'ACK':
+                aAejecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + FICHERO
+                print 'Vamos a ejecutar', aAejecutar
+                os.system(aAejecutar)
+            elif Metod == 'BYE':
+                self.wfile.write('SIP/2.0 200 OK' + '\r\n\r\n')
+                print 'Terminando conversación...'
+            else:
+                self.wfile.write('SIP/2.0 400 Bad Request' + '\r\n\r\n')
+
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    Metodos = ['INVITE', 'ACK', 'BYE']
+    serv = SocketServer.UDPServer(("", PUERTO), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
