088291e4d93253fa5ec7dac0ca027d6386594f7c
alvaromv83 <alvaromv83@gmail.com>
2014-11-24 12:31:15 +0100

    Depurado server.py

--- server.py
+++ server.py
@@ -33,19 +33,37 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             else:
                 # Evaluación de los parámetros que nos envía el cliente
                 print "Recibido:\n" + line
-                parameters = line.split()
-                method = parameters[0]
-                user = parameters[1].split(':')[1]
-                version = parameters[2]
+                try:
+                    parameters = line.split()
+                    method = parameters[0]
+                    protocol = parameters[1].split(':')[0]
+                    address = parameters[1].split(':')[1]
+                    user = address.split('@')[0]
+                    ip = address.split('@')[1]
+                    client_version = parameters[2]
+                    if protocol != 'sip' or client_version != 'SIP/1.0'\
+                        and client_version != 'SIP/2.0':
+                        response = version + " 400 Bad Request\r\n\r\n"
+                        self.wfile.write(response)
+                        print "Enviado:\n" + response
+                        break
+                except:
+                    response = version + " 400 Bad Request\r\n\r\n"
+                    self.wfile.write(response)
+                    print "Enviado:\n" + response
+                    break
 
                 # Evaluación del método que nos envía el cliente
                 if method == 'INVITE':
                     response = version + " 100 Trying\r\n\r\n"
-                    response += version + " 180 Ring\r\n\r\n"
+                    response += version + " 180 Ringing\r\n\r\n"
                     response += version + " 200 OK\r\n\r\n"
                     self.wfile.write(response)
+                    print "Enviado:\n" + response
                 elif method == 'BYE':
-                    self.wfile.write(version + " 200 OK\r\n\r\n")
+                    response = version + " 200 OK\r\n\r\n"
+                    self.wfile.write(response)
+                    print "Enviado:\n" + response
                 elif method == 'ACK':
                     # --------------------- Envío RTP -------------------------
                     toRun = "./mp32rtp -i 127.0.0.1 -p 23032 < " + AUDIO_FILE
@@ -53,11 +71,15 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                     os.system(toRun)
                     print "Finalizado envío RTP"
                 else:
-                    self.wfile.write(version +
-                                     " 405 Method Not Allowed\r\n\r\n")
+                    response = version + " 405 Method Not Allowed\r\n\r\n"
+                    self.wfile.write(response)
+                    print "Enviado:\n" + response
 
 
 if __name__ == "__main__":
+
+    # Versión del protocolo SIP
+    version = "SIP/2.0"
 
     # Evaluación de parámetros de la línea de comandos
     try:
