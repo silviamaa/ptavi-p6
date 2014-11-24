0491e79ecac1b1139977445cc206b6974519f7fc
mireya elisabet valenzuela carballo <mireya@iota13.aulas.gsyc.es>
2014-11-24 10:16:13 +0100

    funciona menos rtp

--- client.py
+++ client.py
@@ -3,30 +3,48 @@
 """
 Programa cliente que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try: 
+    UA = sys.argv[0] = 'client.py'
+    METOD = sys.argv[1]
+    RECEPTOR = sys.argv[2].split('@')[0]
+    IP_R = sys.argv[2].split('@')[1].split(':')[0]
+    PUERTO_R = int(sys.argv[2].split('@')[1].split(':')[1])
+except IndexError:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = METOD + ' sip:' + RECEPTOR + '@' + IP_R + ' SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP_R, PUERTO_R))
 
 print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+my_socket.send(LINE)
+try:
+    data = my_socket.recv(1024)
+except socket.error:
+    sys.exit("Error: No server listening at " + IP_R + " port " + str(PUERTO_R))
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+print 'Recibido -- ', '\r\n\r\n', data
+rcv_invite = data.split('\r\n\r\n')[1:-1]
+
+if rcv_invite == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring', 'SIP/2.0 200 OK']:
+    METOD = 'ACK'
+    print "Enviando ACK..."
+    NEWLINE =  METOD + ' sip:' + RECEPTOR + '@' + IP_R + ' SIP/2.0\r\n\r\n'
+    my_socket.send(NEWLINE)
+    data = my_socket.recv(1024)
+    print data
 
 # Cerramos todo
 my_socket.close()
-print "Fin."
+
+
