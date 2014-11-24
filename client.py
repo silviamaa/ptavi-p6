f035e3d0319c304417a1f07fcf9d727aaef87557
alvaromv83 <alvaromv83@gmail.com>
2014-11-24 12:31:04 +0100

    Depurado client.py

--- client.py
+++ client.py
@@ -12,21 +12,24 @@ import sys
 # Dirección IP del servidor.
 SERVER = 'localhost'
 
+# Protocolo y versión
+PROTOCOL = "sip"
+VERSION = "SIP/2.0"
+
 # Parámetros del usuario.
 try:
-    METHOD = sys.argv[1].upper()
+    method = sys.argv[1].upper()
     parameters = sys.argv[2]
 
-    USER = parameters.split(':')[0]
-    IP = USER.split('@')[1]
+    ADDRESS = parameters.split(':')[0]
+    IP = ADDRESS.split('@')[1]
     PORT = int(parameters.split(':')[1])
-    VERSION = "SIP/2.0"
 except:
     print ("Usage: python client.py method receiver@IP:SIPport")
     raise SystemExit
 
 # Contenido que vamos a enviar
-request = METHOD + " " + "sip:" + USER + " " + VERSION + "\r\n\r\n"
+request = method + " " + PROTOCOL + ":" + ADDRESS + " " + VERSION + "\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,8 +37,8 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 # Enviamos solicitud y recibimos respuesta
-print "Enviando:\n" + request
 my_socket.send(request)
+print "Enviado:\n" + request
 try:
     data = my_socket.recv(1024)
 except socket.error:
@@ -45,16 +48,20 @@ except socket.error:
 print 'Recibido:\n' + data
 
 # Si recibimos confirmación de INVITE envíamos ACK
-response = VERSION + " 100 Trying\r\n\r\n"
-response += VERSION + " 180 Ring\r\n\r\n"
-response += VERSION + " 200 OK\r\n\r\n"
-if data == response:
-    request = "ACK " + "sip:" + USER + " " + VERSION + "\r\n\r\n"
-    print "Enviando:\n" + request
-    my_socket.send(request)
+response1 = "SIP/1.0 100 Trying\r\n\r\n" + "SIP/1.0 180 Ringing\r\n\r\n"\
+          + "SIP/1.0 200 OK\r\n\r\n"
+response2 = "SIP/2.0 100 Trying\r\n\r\n" + "SIP/2.0 180 Ringing\r\n\r\n"\
+          + "SIP/2.0 200 OK\r\n\r\n"
 
-print "Terminando socket..."
+if data == response1 or data == response2:
+    method = 'ACK'
+    request = method + " " + PROTOCOL + ":" + ADDRESS + " " + VERSION\
+              + "\r\n\r\n"
+    my_socket.send(request)
+    print "Enviado:\n" + request
+
+print "Finalizando socket..."
 
 # Cerramos todo
 my_socket.close()
-print "Fin."
+print "Socket finalizado."
