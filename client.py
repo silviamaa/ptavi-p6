0332ae6aa80107791cd213b7a766e432ec8ac292
mireya elisabet valenzuela carballo <mireya@iota13.aulas.gsyc.es>
2014-11-24 10:39:14 +0100

    codigo bien

--- client.py
+++ client.py
@@ -6,10 +6,8 @@ Programa cliente que abre un socket a un servidor
 import sys
 import socket
 
-# Cliente UDP simple.
 
-# Dirección IP del servidor.
-try: 
+try:
     UA = sys.argv[0] = 'client.py'
     METOD = sys.argv[1]
     RECEPTOR = sys.argv[2].split('@')[0]
@@ -26,25 +24,22 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP_R, PUERTO_R))
 
-print "Enviando: " + LINE
+print '\r\n\r\n' + "Enviando: " + LINE
 my_socket.send(LINE)
 try:
     data = my_socket.recv(1024)
 except socket.error:
     sys.exit("Error: No server listening at " + IP_R + " port " + str(PUERTO_R))
 
-print 'Recibido -- ', '\r\n\r\n', data
-rcv_invite = data.split('\r\n\r\n')[1:-1]
-
+print data
+rcv_invite = data.split('\r\n\r\n')[0:-1]
 if rcv_invite == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring', 'SIP/2.0 200 OK']:
     METOD = 'ACK'
-    print "Enviando ACK..."
     NEWLINE =  METOD + ' sip:' + RECEPTOR + '@' + IP_R + ' SIP/2.0\r\n\r\n'
+    print '\r\n\r\n' + "Enviando: " + NEWLINE
     my_socket.send(NEWLINE)
     data = my_socket.recv(1024)
     print data
 
 # Cerramos todo
 my_socket.close()
-
-
